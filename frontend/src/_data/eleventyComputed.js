5e17029a617572e9b9f28665ee0ac37ee82320c1
Miguel Vieira <jmvieira@pm.me>
2022-10-11 15:49:34 +0100

    👌 Improve: Add computed data for SEO.

--- frontend/src/_data/eleventyComputed.js
+++ frontend/src/_data/eleventyComputed.js
@@ -1,5 +1,7 @@
 module.exports = {
-  navKey: (data) => {
-    return data.eleventyNavigation?.key || data.navKey;
-  },
+  // SEO plugin
+  author: (data) => data.authors?.join(", "),
+  image: (data) => data.feature?.image,
+  // SEO plugin end
+  navKey: (data) => data.eleventyNavigation?.key || data.navKey,
 };
