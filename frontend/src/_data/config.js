d9d58c98f6f8cdbe20dada7a8ea011696917d92b
Miguel Vieira <jmvieira@pm.me>
2022-11-03 15:54:28 +0000

    📦 New: Add data with global config for the site.

---
+++ frontend/src/_data/config.js
@@ -0,0 +1,26 @@
+const baseUrl = "/";
+const paths = {
+  assets: `${baseUrl}assets`,
+  images: `${baseUrl}assets/images`,
+  stylesheets: `${baseUrl}assets/stylesheets`,
+};
+
+module.exports = {
+  baseUrl: baseUrl,
+  title: "King's Digital Lab",
+  description:
+    "King’s Digital Lab (KDL) is a Research Software Engineering (RSE)...",
+  url: "https://kdl.kcl.ac.uk",
+  author: "King's Digital Lab",
+  twitter: "kingsdigitallab",
+  feature: {
+    image: `${paths.images}/kings-logo-red.svg`,
+    description: "King's College London log in red",
+  },
+  // SEO plugin options
+  options: {
+    titleDivider: "|",
+    imageWithBaseUrl: true,
+  },
+  paths: { ...paths },
+};
