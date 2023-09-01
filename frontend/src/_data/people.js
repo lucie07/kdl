873e09327e089368a08eb941379bc9e2f47c665c
Miguel Vieira <jmvieira@pm.me>
2023-09-01 19:39:03 +0100

    fix(frontend): exclude people that don't have an agent object

--- frontend/src/_data/people.js
+++ frontend/src/_data/people.js
@@ -1,18 +1,5 @@
 const helpers = require("./_helpers");
 
-const callback = (data) =>
-  data.map((d) => {
-    const orgs = d.agent.memberOf
-      .filter((m) => m.inOrganisation?.agent.name === "King's Digital Lab")
-      .map((m) => ({
-        role: m.roleName.name,
-        startYear: m.startDate ? new Date(m.startDate).getFullYear() : null,
-        endYear: m.endDate ? new Date(m.endDate).getFullYear() : null,
-      }));
-
-    return { memberOfKDL: orgs, ...d };
-  });
-
 module.exports = async ({ directus }) => {
   return helpers.loadData(
     directus,
@@ -38,6 +25,6 @@ module.exports = async ({ directus }) => {
       "agent.memberOf.endDate",
     ],
     "agent.name",
-    callback
+    (data) => data.filter((person) => person.agent)
   );
 };
